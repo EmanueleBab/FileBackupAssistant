e55cb4b6d75f4ce119dbbf9cbff3bb513ca0787a
emanuele babolo <emanuelebabolo@outlook.it>
2020-09-04 22:22:14 +0200

    first version

---
+++ FileBackupByGigginoCorp/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿using System;
+
+namespace FileBackupByGigginoCorp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.TargetStrip = new System.Windows.Forms.ToolStripMenuItem();
+            this.DestinationStrip = new System.Windows.Forms.ToolStripMenuItem();
+            this.ApplyStrip = new System.Windows.Forms.ToolStripMenuItem();
+            this.Menu = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.TargetStrip,
+            this.DestinationStrip,
+            this.ApplyStrip});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(172, 70);
+            // 
+            // TargetStrip
+            // 
+            this.TargetStrip.Name = "TargetStrip";
+            this.TargetStrip.Size = new System.Drawing.Size(171, 22);
+            this.TargetStrip.Text = "Directory to save";
+            this.TargetStrip.Click += new System.EventHandler(this.DirToSave);
+            // 
+            // DestinationStrip
+            // 
+            this.DestinationStrip.Name = "DestinationStrip";
+            this.DestinationStrip.Size = new System.Drawing.Size(171, 22);
+            this.DestinationStrip.Text = "Destination folder ";
+            this.DestinationStrip.Click += new System.EventHandler(this.TargetDir);
+            // 
+            // ApplyStrip
+            // 
+            this.ApplyStrip.Name = "ApplyStrip";
+            this.ApplyStrip.Size = new System.Drawing.Size(171, 22);
+            this.ApplyStrip.Text = "Apply ";
+            this.ApplyStrip.Click += new System.EventHandler(this.applicamodificheToolStripMenuItem_Click);
+            // 
+            // Menu
+            // 
+            this.Menu.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.Menu.BalloonTipText = "a";
+            this.Menu.BalloonTipTitle = "a";
+            this.Menu.ContextMenuStrip = this.contextMenuStrip1;
+            this.Menu.Icon = ((System.Drawing.Icon)(resources.GetObject("Menu.Icon")));
+            this.Menu.Text = "FileBackupper";
+            this.Menu.Visible = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(596, 325);
+            this.ControlBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "GIGGICORP";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+   
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem TargetStrip;
+        private System.Windows.Forms.ToolStripMenuItem DestinationStrip;
+        private System.Windows.Forms.ToolStripMenuItem ApplyStrip;
+        private System.Windows.Forms.NotifyIcon Menu;
+    }
+}
+
---
+++ FileBackupByGigginoCorp/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.IO;
+using System.Security.Permissions;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace FileBackupByGigginoCorp
+{
+    public partial class Form1 : Form
+    {
+        public static string fileToSave;
+        public static string destination;
+        public static bool canSave, running = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Initialize();
+            RunThread();
+        }
+
+        private void RunThread()
+        {
+            if(canSave && !running)
+            {
+                Thread thread1 = new Thread(Run);
+                thread1.Start();
+            }
+        }
+
+        private void applicamodificheToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if ((destination != null && fileToSave != null))
+            {
+                StreamWriter sw = File.CreateText(@".\data.txt");
+
+                canSave = true;
+                sw.WriteLine(fileToSave);
+                sw.WriteLine(destination);
+                sw.Close();
+                RunThread();
+
+            }
+            else
+            {
+                MessageBox.Show("error a directory hasn't been selected", "error", MessageBoxButtons.OK);
+            }
+        }
+
+        private void rICORDAGIGGIODIAGESTIREGLIERRORINELLEAPPACASOQUINDINONFARESTRONZATEToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private static void Initialize()
+        {
+            if (!File.Exists(@".\data.txt"))
+            {
+                using (StreamWriter sw = File.AppendText(@".\data.txt"))
+                {
+                    canSave = false;
+                    sw.Close();
+                }
+            }
+            try
+            {
+                using (StreamReader sr = new StreamReader(@".\data.txt"))
+                {
+                    fileToSave = sr.ReadLine();
+                    if (fileToSave == null)
+                    {
+                        canSave = false;
+                        return;
+                    }
+                    destination = sr.ReadLine();
+                    canSave = true;
+                }
+            }
+            catch
+            {
+                canSave = false;
+            }
+
+        }
+
+        [PermissionSet(SecurityAction.Demand, Name = "FullTrust")]
+        private static void Run()
+        {
+            if (!running)
+            {
+                running = true;
+                // Create a new FileSystemWatcher and set its properties.
+                using (FileSystemWatcher watcher = new FileSystemWatcher())
+                {
+                    watcher.Path = fileToSave;
+
+                    // Watch for changes in LastAccess and LastWrite times, and
+                    // the renaming of files or directories.
+                    watcher.NotifyFilter = NotifyFilters.LastAccess
+                                         | NotifyFilters.LastWrite
+                                         | NotifyFilters.FileName
+                                         | NotifyFilters.DirectoryName;
+
+                    // Only watch text files.
+
+                    // Add event handlers.
+                    watcher.Changed += OnChanged;
+                    watcher.Created += OnChanged;
+
+
+                    // Begin watching.
+                    watcher.EnableRaisingEvents = true;
+
+                    // Wait for the user to quit the program.
+                    while (true) ;
+                }
+            }
+
+        }
+
+        // Define the event handlers.
+        private static void OnChanged(object source, FileSystemEventArgs e)
+        {
+            if (canSave)
+            {
+
+                // Specify what is done when a file is changed, created, or deleted.
+
+
+                string[] files = System.IO.Directory.GetFiles(fileToSave);
+
+                // Copy the files and overwrite destination files if they already exist.
+                foreach (string s in files)
+                {
+                    // Use static Path methods to extract only the file name from the path.
+                    string fileName = System.IO.Path.GetFileName(s);
+                    string destFile = System.IO.Path.Combine(destination, fileName);
+                    System.IO.File.Copy(s, destFile, true);
+                }
+            }
+        }
+
+        private void DirToSave(object sender, EventArgs e)
+        {
+
+            destination = GetFile();
+            canSave = false;
+        }
+
+       
+
+        private void TargetDir(object sender, EventArgs e)
+        {
+            fileToSave = GetFile();
+
+            canSave = false;
+
+        }
+
+
+        public string GetFile()
+        {
+            FolderBrowserDialog browser = new FolderBrowserDialog();
+
+            if (browser.ShowDialog() == DialogResult.OK)
+            {
+                return browser.SelectedPath;
+            }
+            return null;
+        }
+    }
+}
